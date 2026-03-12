5ac5498a6da912dd261810061d14a1c91b64e145
Gerard Causse <grod.causse@gmail.com>
2026-03-11 22:35:45 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,17 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept()  # wait for client
     request = connection.recv(4_096).decode()
-    request_lines = request.split("\r\n")
-    request_line = request_lines[0]
+    request_lines_and_body = request.split("\r\n\r\n")
+    body = None
+    if len(request_lines_and_body) == 2:
+        body = request_lines_and_body[1]
+    request_lines = request_lines_and_body[0].split("\r\n")
+    request_line, headers = request_lines[0], request_lines[1:]
+    user_agent = None
+    for header in headers:
+        key, value = header.split()
+        if key.lower() == "user-agent:":
+            user_agent = value
     request_line_parts = request_line.split()
     method, target, version = (
         request_line_parts[0],
@@ -22,6 +31,8 @@ def main():
         echo = target[6:]
         message = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}"
         connection.sendall(message.encode())
+    elif target.startswith("/user-agent") and user_agent:
+        message = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
     else:
         connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
