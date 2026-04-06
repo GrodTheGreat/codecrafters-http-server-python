4c1bf8a8ff6263ed971a80f18611a8e1888a43cc
Gerard Causse <gcausse@epri.com>
2026-04-06 12:11:03 -0400

    Update shell scripts to reflect new project structure

---
+++ src/main.py
@@ -0,0 +1,157 @@
+import asyncio
+import gzip
+import sys
+from asyncio import StreamReader, StreamWriter
+from pathlib import Path
+
+from exceptions import BadRequestException
+from parser import Parser
+from protocol import (
+    HttpResponse,
+    HttpStatusLine,
+    HttpVersion,
+    HttpStatus,
+    HttpRequest,
+    HttpMethod,
+    HttpHeaders,
+)
+
+
+def not_found() -> HttpResponse:
+    return HttpResponse(
+        HttpStatusLine(HttpVersion.V1_1, HttpStatus.NOT_FOUND),
+        HttpHeaders(),
+        None,
+    )
+
+
+def bad_request() -> HttpResponse:
+    return HttpResponse(
+        HttpStatusLine(HttpVersion.V1_1, HttpStatus.BAD_REQUEST),
+        HttpHeaders(),
+        None,
+    )
+
+
+def internal_server_error() -> HttpResponse:
+    return HttpResponse(
+        HttpStatusLine(HttpVersion.V1_1, HttpStatus.INTERNAL_SERVER_ERROR),
+        HttpHeaders(),
+        None,
+    )
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(
+        HttpStatusLine(HttpVersion.V1_1, HttpStatus.OK), HttpHeaders(), None,
+    )
+
+
+def echo(request: HttpRequest) -> HttpResponse:
+    param = request.request_line.target.path[6:]
+    encoding = request.headers.get("accept-encoding")
+    headers = HttpHeaders()
+    if encoding and "gzip" in encoding:
+        headers["content-encoding"] = ["gzip"]
+        param = gzip.compress(param.encode())
+    headers["content-type"] = ["text/plain"]
+    headers["content-length"] = [str(len(param))]
+    if isinstance(param, str):
+        param = param.encode()
+    return HttpResponse(HttpStatusLine(HttpVersion.V1_1, HttpStatus.OK), headers, param)
+
+
+def user_agent(request: HttpRequest) -> HttpResponse:
+    if "user-agent" not in request.headers:
+        raise BadRequestException()
+    agent = request.headers["user-agent"]
+    headers = HttpHeaders()
+    headers["content-type"] = ["text/plain"]
+    headers["content-length"] = [str(len(agent))]
+    return HttpResponse(
+        HttpStatusLine(HttpVersion.V1_1, HttpStatus.OK), headers, agent[0],
+    )
+
+
+FILES_DIR = "/tmp/"
+
+
+def get_files(request: HttpRequest) -> HttpResponse:
+    try:
+        file = request.request_line.target.path[7:]
+    except IndexError:
+        raise BadRequestException()
+    filepath = Path(Path(FILES_DIR) / file).resolve()
+    if not filepath.is_relative_to(Path(FILES_DIR).resolve()):
+        return not_found()
+    if not filepath.exists():
+        return not_found()
+    with open(filepath, "rb") as f:
+        data = f.read()
+    headers = HttpHeaders()
+    headers["content-type"] = ["application/octet-stream"]
+    headers["content-length"] = [str(len(data))]
+
+    return HttpResponse(HttpStatusLine(HttpVersion.V1_1, HttpStatus.OK), headers, data)
+
+
+def post_files(request: HttpRequest) -> HttpResponse:
+    try:
+        filename = request.request_line.target.path[7:]
+    except IndexError:
+        raise BadRequestException()
+    filepath = Path(Path(FILES_DIR) / filename).resolve()
+    if not filepath.is_relative_to(Path(FILES_DIR).resolve()):
+        raise BadRequestException()
+    if request.body is None or not request.body:
+        raise BadRequestException("body is required for this request")
+    if isinstance(request.body, str):
+        with open(filepath, "w") as f:
+            f.write(request.body)
+    if isinstance(request.body, bytes):
+        with open(filepath, "wb") as f:
+            f.write(request.body)
+    return HttpResponse(
+        HttpStatusLine(HttpVersion.V1_1, HttpStatus.CREATED), HttpHeaders(), None,
+    )
+
+
+async def handle_connection(reader: StreamReader, writer: StreamWriter):
+    router = {
+        (HttpMethod.GET, "/"): index,
+        (HttpMethod.GET, "/echo"): echo,
+        (HttpMethod.GET, "/user-agent"): user_agent,
+        (HttpMethod.GET, "/files"): get_files,
+        (HttpMethod.POST, "/files"): post_files,
+    }
+    parser = Parser(reader)
+    while not reader.at_eof():
+        request = await parser.parse()
+    pass
+
+
+async def main() -> None:
+    global FILES_DIR
+    print("Logs from your program will appear here!")
+
+    args = sys.argv
+    if "--directory" in args:
+        dir_index = args.index("--directory")
+        FILES_DIR = args[dir_index + 1]
+
+    server = await asyncio.start_server(
+        handle_connection,
+        host="localhost",
+        port=4221,
+        reuse_port=True,
+    )
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nserver shutting down...")
+        print("have a good day :)")
